2734f47e0a7b20e31d0dce17e78e89ac9777d054
AVasK <avask17@gmail.com>
2017-04-21 23:07:19 +0300

    Initial Commit

---
+++ e-store/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  e-store
+//
+//  Created by Alex Vaskov on 21.04.17.
+//  Copyright © 2017 CodeProject. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
